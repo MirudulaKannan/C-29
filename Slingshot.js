3a4db00f8a412962857b2abe969512ce831b4b40
Mirudula Kannan <pinkybrinda6@gmail.com>
2020-10-29 17:58:23 +0530

    catapult

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.slingShot1 = loadImage("sprites/sling1.png");
+        this.slingShot2 = loadImage("sprites/sling2.png");
+        this.slingShot3 = loadImage("sprites/sling3.png")
         World.add(world, this.sling);
     }
 
@@ -16,12 +19,28 @@ class SlingShot{
     }
 
     display(){
+        image(this.slingShot1,200,20);
+        image(this.slingShot2,170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x <= 200){
+                strokeWeight(4);
+                stroke(color(48,22,8))
+                line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+                image(this.slingShot3,pointA.x-30,pointA.y-10,15,25);
+            }
+            else{
+                strokeWeight(4);
+                stroke(color(48,22,8))
+                line(pointA.x+20, pointA.y, pointB.x-20, pointB.y);
+                line(pointA.x+20, pointA.y, pointB.x+20, pointB.y);
+                image(this.slingShot3,pointA.x+20,pointA.y-10,15,25);
+            }
+
         }
+
     }
     
 }
